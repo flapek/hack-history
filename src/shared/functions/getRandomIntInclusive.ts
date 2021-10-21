1061e7bf9e10af5e7e3726e09f17246fbc7b62b4
Flapek <filap4@gmail.com>
2021-10-21 20:57:47 +0200

    throw error if variables is wrong

--- src/shared/functions/getRandomIntInclusive.ts
+++ src/shared/functions/getRandomIntInclusive.ts
@@ -1,4 +1,5 @@
-function getRandomIntInclusive(min: number, max: number) {
+function getRandomIntInclusive(min: number, max: number): number {
+  if (min > max) throw new Error("Min value is higher then max!");
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
